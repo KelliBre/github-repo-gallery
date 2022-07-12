7be1dbf3ec991045f25740bf9df79397dd64f6b6
KelliBre <Kelli.Brecunier@gmail.com>
2022-07-12 13:08:45 +1000

    repo list element updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //global
 const overview = document.querySelector(".overview");
 const username= "KelliBre";//github user name
+const repoList = document.querySelector(".repo-list");
 
 //async function to fetch github profile
 const getGithubUser = async function(){
@@ -28,4 +29,22 @@ const displayMyInfo = function(data){
     </div>
   `;
   overview.append(div);
+  userRepoList();
 };
+
+const userRepoList = async function(){
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    //console.log(repoData);
+    displayRepoInfo(repoData);
+
+}
+
+const displayRepoInfo = function(repos){
+    for (const repo of repos){
+        const repoItem =document.createElement('li');
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3> ${repo.name} </h3>`
+        repoList.append(repoItem);
+    }
+}
